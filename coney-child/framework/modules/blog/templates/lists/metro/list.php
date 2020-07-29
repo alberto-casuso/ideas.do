15cb0a85b151ac2d3ae851b5a88f090271524c13
Yumilitza Mancilla <mcyumilitza@gmail.com>
2020-07-29 08:18:25 -0500

    added post.php in theme child

--- coney-child/framework/modules/blog/templates/lists/metro/list.php
+++ coney-child/framework/modules/blog/templates/lists/metro/list.php
@@ -4,21 +4,7 @@
         <div class="qodef-blog-masonry-grid-gutter"></div>
         <?php
             if($blog_query->have_posts()) : while ( $blog_query->have_posts() ) : $blog_query->the_post();
-                //coney_qodef_get_post_format_html($blog_type);
-        ?>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm">
-                        One of three columns
-                        </div>
-                        <div class="col-sm">
-                        One of three columns
-                        </div>
-                    </div>
-                </div>
-
-        <?php
-
+                coney_qodef_get_post_format_html($blog_type);
             endwhile;
             else:
                 coney_qodef_get_module_template_part('templates/parts/no-posts', 'blog');
---
+++ coney-child/framework/modules/blog/templates/lists/metro/post.php
@@ -0,0 +1,78 @@
+<?php
+$image_proportion = coney_qodef_get_meta_field_intersect('blog_list_featured_image_proportion', coney_qodef_get_page_id());
+$image_proportion_value = get_post_meta(get_the_ID(), 'qodef_blog_masonry_gallery_' . $image_proportion .'_dimensions_meta', true);
+
+if (isset($image_proportion_value) && $image_proportion_value !== '') {
+    $size = $image_proportion_value !== '' ? $image_proportion_value : 'default';
+    $post_classes[] = 'qodef-post-size-'. $size;
+}
+else {
+    $size = 'default';
+    $post_classes[] = 'qodef-post-size-default';
+}
+if($image_proportion == 'original') {
+    $part_params['featured_image_size'] = 'full';
+}
+else {
+    switch ($size):
+        case 'default':
+            $part_params['featured_image_size'] = 'coney_qodef_square';
+            break;
+        case 'large-width':
+            $part_params['featured_image_size'] = 'coney_qodef_landscape';
+            break;
+        case 'large-height':
+            $part_params['featured_image_size'] = 'coney_qodef_portrait';
+            break;
+        case 'large-width-height':
+            $part_params['featured_image_size'] = 'coney_qodef_huge';
+            break;
+        default:
+            $part_params['featured_image_size'] = 'coney_qodef_square';
+            break;
+    endswitch;
+}
+?>
+<!--article id="post-< ?php the_ID(); ?>" < ?php post_class($post_classes); ?>>
+        < ?php coney_qodef_get_module_template_part('templates/parts/image', 'blog', '', $part_params); ?>
+
+        <-?php coney_qodef_get_module_template_part('templates/parts/post-info/link-overlay', 'blog'); ?>
+
+        <div class="qodef-post-info texto-blog-home">
+            <div class="qodef-post-top-section">
+                <-?php coney_qodef_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
+                <-?php coney_qodef_get_module_template_part('templates/parts/post-info/category', 'blog', '', $part_params); ?>
+            </div>
+            <div class="qodef-post-text-main">
+            <-?php coney_qodef_get_module_template_part('templates/parts/title', 'blog', '', $part_params); ?>
+            </div>
+            <div class="qodef-post-info-bottom">
+                <-?php coney_qodef_get_module_template_part('templates/parts/post-info/read-more', 'blog', '', $part_params); ?>
+            </div>
+        </div>
+
+</article-->
+<article id="post-<?php the_ID(); ?>" <?php post_class($post_classes); ?>>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <?php coney_qodef_get_module_template_part('templates/parts/post-info/link-overlay', 'blog'); ?>
+                <div class="qodef-post-info texto-blog-home">
+                    <div class="qodef-post-top-section">
+                        <?php coney_qodef_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
+                        <?php coney_qodef_get_module_template_part('templates/parts/post-info/category', 'blog', '', $part_params); ?>
+                    </div>
+                    <div class="qodef-post-text-main">
+                    <?php coney_qodef_get_module_template_part('templates/parts/title', 'blog', '', $part_params); ?>
+                    </div>
+                    <div class="qodef-post-info-bottom">
+                        <?php coney_qodef_get_module_template_part('templates/parts/post-info/read-more', 'blog', '', $part_params); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+            <?php coney_qodef_get_module_template_part('templates/parts/image', 'blog', '', $part_params); ?>
+            </div>
+        </div>
+    </div>
+</article>
