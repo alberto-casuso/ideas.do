fffa9da07d25c36ed857f124e35e0c39e00b8f13
Yumilitza Mancilla <mcyumilitza@gmail.com>
2020-08-07 19:51:02 -0500

    cambiando shortcode de sliderREv

--- coney-child/slider-custom-pmi.php
+++ coney-child/slider-custom-pmi.php
@@ -3,9 +3,9 @@ do_action('coney_qodef_before_slider_action');
 
 //$qodef_slider_shortcode = get_post_meta(coney_qodef_get_page_id(), 'qodef_page_slider_meta', true);
 if (!empty($qodef_slider_shortcode)) { ?>
-	<div class="qodef-slider">
-		<div class="qodef-slider-inner">
-			<?php echo do_shortcode(wp_kses_post($qodef_slider_shortcode)); // XSS OK ?>
+	<div class="qodef-slider slide-mcyy">
+		<div class="qodef-slider-inner slide-mcyy">
+			<?php echo do_shortcode('[rev_slider alias="front-page"]'); // XSS OK ?>
 		</div>
 	</div>
 <?php }
