dcb20aa985f0dbfe0e10fd90c770150487906238
Yumilitza Mancilla <mcyumilitza@gmail.com>
2020-08-27 00:26:15 -0500

    agregando share en el top

---
+++ coney-child/framework/modules/blog/templates/singles/full-width-image/single.php
@@ -0,0 +1,16 @@
+<?php
+coney_qodef_get_single_post_format_html($blog_single_type);
+?>
+<div class="qodef-container">
+    <div class="qodef-container-inner clearfix">
+        <?php
+        coney_qodef_get_module_template_part('templates/parts/single/author-info', 'blog');
+
+        coney_qodef_get_module_template_part('templates/parts/single/single-navigation', 'blog');
+
+        coney_qodef_get_module_template_part('templates/parts/single/related-posts', 'blog', $related_posts_layout, $single_info_params);
+
+        coney_qodef_get_module_template_part('templates/parts/single/comments', 'blog');
+        ?>
+</div>
+</div>
