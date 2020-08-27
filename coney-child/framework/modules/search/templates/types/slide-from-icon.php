91e50e2bd514fd7993271ebcc128bc4aaefaa191
Yumilitza Mancilla <mcyumilitza@gmail.com>
2020-08-26 22:10:19 -0500

    cambiando textos a español para search

---
+++ coney-child/framework/modules/search/templates/types/slide-from-icon.php
@@ -0,0 +1,7 @@
+<div class="qodef-slide-from-icon-holder">
+    <form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+	    <span class="qodef-form-holder">
+	        <input type="text" placeholder="<?php esc_attr_e( 'Buscar', 'coney' ); ?>" name="s" class="qodef-search-field" autocomplete="off"/>
+	    </span>
+    </form>
+</div>
